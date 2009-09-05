74554899f004009635ac3a55d3e63ada52132387
Brian D'Souza <bmale@hotmal.com>
2009-09-04 23:07:40 -0600

    this is bad code!

--- FOWL/src/fowl.py
+++ FOWL/src/fowl.py
@@ -21,117 +21,129 @@ mat_shininess = [ 5.0 ]
 
 class MainWindow(window.Window):
 
-	def __init__(self, *args, **kwargs):
-		#Let all of the standard stuff pass through
-		window.Window.__init__(self, *args, **kwargs)
-		self.initGL()
-		self.bird = Bird()
-		self.world = World()
+    def __init__(self, *args, **kwargs):
+        #Let all of the standard stuff pass through
+        window.Window.__init__(self, *args, **kwargs)
+        self.initGL()
+        self.bird = Bird()
+        self.world = World()
 
 
-	def initGL(self):
+    def initGL(self):
 
-		glClearColor(0.0, 0.0, 0.0, 0.0)        # This Will Clear The Background Color To Black
-		glClearDepth(1.0)                                       # Enables Clearing Of The Depth Buffer
-		glDepthFunc(GL_LESS)                            # The Type Of Depth Test To Do
-		glEnable(GL_DEPTH_TEST)                         # Enables Depth Testing
-		glShadeModel(GL_SMOOTH)                         # Enables Smooth Color Shading
-		
-		glEnable(GL_TEXTURE_2D)               # Enable Texture Mapping
-		glShadeModel(GL_SMOOTH)               # Enable Smooth Shading
-		glClearColor(0.0, 0.0, 0.0, 0.5)      # Black Background
-		glClearDepth(1.0)                     # Depth Buffer Setup
-		glEnable(GL_DEPTH_TEST)               # Enables Depth Testing
-		glDepthFunc(GL_LEQUAL)                # The Type Of Depth Testing To Do
-		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST) # Really Nice Perspective Calculations
-		
-		glLightfv(GL_LIGHT1, GL_AMBIENT, vec(*LightAmbient))
-		glLightfv(GL_LIGHT1, GL_DIFFUSE, vec(*LightDiffuse))
-		glLightfv(GL_LIGHT1, GL_POSITION, vec(*LightPosition))
-		glEnable(GL_LIGHT1)
+        glClearColor(0.0, 0.0, 0.0, 0.0)        # This Will Clear The Background Color To Black
+        glClearDepth(1.0)                                       # Enables Clearing Of The Depth Buffer
+        glDepthFunc(GL_LESS)                            # The Type Of Depth Test To Do
+        glEnable(GL_DEPTH_TEST)                         # Enables Depth Testing
+        glShadeModel(GL_SMOOTH)                         # Enables Smooth Color Shading
+        
+        glEnable(GL_TEXTURE_2D)               # Enable Texture Mapping
+        glShadeModel(GL_SMOOTH)               # Enable Smooth Shading
+        glClearColor(0.0, 0.0, 0.0, 0.5)      # Black Background
+        glClearDepth(1.0)                     # Depth Buffer Setup
+        glEnable(GL_DEPTH_TEST)               # Enables Depth Testing
+        glDepthFunc(GL_LEQUAL)                # The Type Of Depth Testing To Do
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST) # Really Nice Perspective Calculations
+        
+        glLightfv(GL_LIGHT1, GL_AMBIENT, vec(*LightAmbient))
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, vec(*LightDiffuse))
+        glLightfv(GL_LIGHT1, GL_POSITION, vec(*LightPosition))
+        glEnable(GL_LIGHT1)
+        
+        glColor4f(1.0,1.0,1.0,0.5)
+        glBlendFunc(GL_SRC_ALPHA,GL_ONE)
 
-		glColor4f(1.0,1.0,1.0,0.5)
-		glBlendFunc(GL_SRC_ALPHA,GL_ONE)
+    def rotate_world(self, interval):
+        speed = 1
+        norm = math.sqrt(self.bird.dest_x*self.bird.dest_x + self.bird.dest_y*self.bird.dest_y)
+        if (norm):
+            #self.world.rotx = speed*self.bird.dest_y/norm
+            #self.world.roty = speed*-self.bird.dest_x/norm
+            self.world.rotate(speed*self.bird.dest_y/norm, -speed*self.bird.dest_x/norm)
+        #self.world.rotate(1*self.bird.dest_y, 1*-self.bird.dest_x)
+        
+    def animate_bird(self, interval):
+        self.bird.flap()
+        #self.bird.move()
 
-	def rotate_bird(self, interval):
-		self.bird.flap()
-		self.bird.move()
-		
-		
-	def main_loop(self):
-		clock.set_fps_limit(30)
-		clock.schedule_interval(self.rotate_bird, 0.1)
+    def update(self):
+        pass
 
-		while not self.has_exit:
-			self.dispatch_events()
-			self.clear()
+    def draw(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glPushMatrix()
+        self.bird.draw()
+        glPopMatrix()
+        
+        glPushMatrix()
+        self.world.draw()
+        glPopMatrix()
+        
+        
+    def main_loop(self):
+        clock.set_fps_limit(30)
+        clock.schedule_interval(self.animate_bird, 0.1)
+        clock.schedule_interval(self.rotate_world, 0.1)
+        
+        while not self.has_exit:
+            self.dispatch_events()
+            self.clear()
+        
+            self.update()
+            self.draw()
+        
+            #Tick the clock
+            clock.tick()
+            self.flip()
 
-			self.update()
-			self.draw()
 
-			#Tick the clock
-			clock.tick()
-			self.flip()
+    
+    """******************************************
+    Event Handlers
+    *********************************************"""
+    def on_mouse_motion(self, x, y, dx, dy):
+        new_dest_x = (float(x - self.width/2.0) / (self.width/2.0))*4.0
+        new_dest_y = (float(y - self.height/2.0) / (self.height/2.0))*4.0
+        old_dest_x = 0#self.bird.dest_x
+        old_dest_y = 0#self.bird.dest_y
 
-	def update(self):
-		pass
+        y_rot = old_dest_y - new_dest_y
+        x_rot = old_dest_x - new_dest_x
 
-	def draw(self):
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-		glLoadIdentity()
-		glPushMatrix()
-		self.bird.draw()
-		glPopMatrix()
-		
-		glPushMatrix()
-		self.world.draw()
-		glPopMatrix()
-	
-	"""******************************************
-	Event Handlers
-	*********************************************"""
-	def on_mouse_motion(self, x, y, dx, dy):
-		new_dest_x = (float(x - self.width/2.0) / (self.width/2.0))*4.0
-		new_dest_y = (float(y - self.height/2.0) / (self.height/2.0))*4.0
-		old_dest_x = 0#self.bird.dest_x
-		old_dest_y = 0#self.bird.dest_y
+        if (y_rot != 0 and new_dest_y < old_dest_y):
+            new_roty_dest = 180 + 1.0 * math.atan((x_rot) /
+                                    (y_rot)) * 180.0/math.pi
+        elif (y_rot != 0):
+            new_roty_dest = 1.0 * math.atan((x_rot) /
+                            (y_rot)) * 180.0/math.pi
 
-		y_rot = old_dest_y - new_dest_y
-		x_rot = old_dest_x - new_dest_x
+        old_dest_x = self.bird.dest_x
+        old_dest_y = self.bird.dest_y
 
-		if (y_rot != 0 and new_dest_y < old_dest_y):
-			new_roty_dest = 180 + 1.0 * math.atan((x_rot) /
-								    (y_rot)) * 180.0/math.pi
-		elif (y_rot != 0):
-			new_roty_dest = 1.0 * math.atan((x_rot) /
-							(y_rot)) * 180.0/math.pi
+        if (old_dest_x < 0 and new_dest_x < 0):
+            if (old_dest_y <= 0 and new_dest_y>=0):
+                new_roty_dest = -90
+                self.bird.roty = -90
+            elif (old_dest_y >=0 and new_dest_y<=0):
+                new_roty_dest = 270
+                self.bird.roty = 270
+            
+        if (y_rot):
+            self.bird.roty_dest=new_roty_dest
 
-		old_dest_x = self.bird.dest_x
-		old_dest_y = self.bird.dest_y
+        self.bird.dest_x = new_dest_x
+        self.bird.dest_y = new_dest_y
 
-		if (old_dest_x < 0 and new_dest_x < 0):
-			if (old_dest_y <= 0 and new_dest_y>=0):
-				new_roty_dest = -90
-				self.bird.roty = -90
-			elif (old_dest_y >=0 and new_dest_y<=0):
-				new_roty_dest = 270
-				self.bird.roty = 270
-			
-		if (y_rot):
-			self.bird.roty_dest=new_roty_dest
-
-		self.bird.dest_x = new_dest_x
-		self.bird.dest_y = new_dest_y
-
-	def on_resize(self, width, height):
-		glViewport(0, 0, width, height)
-		glMatrixMode(GL_PROJECTION)
-		glLoadIdentity()
-		gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)
-		glMatrixMode(GL_MODELVIEW)
+    def on_resize(self, width, height):
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(width)/float(height), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
 
 if __name__ == "__main__":
-	# Someone is launching this directly
-	main = MainWindow()
-	main.main_loop()
+    # Someone is launching this directly
+    main = MainWindow()
+    main.main_loop()
 
--- FOWL/src/world.py
+++ FOWL/src/world.py
@@ -1,22 +1,94 @@
 from pyglet.gl import *
 import math
 from helper import LoadGLTextures
-
-
+import ctypes
+import sys
 
 class World:
     worldTexture=None
     
     def __init__(self):
         self.radius = 15
+        self.rotx=0
+        self.roty=0
+        self.rotz=0
         self.quadratic=gluNewQuadric()
         gluQuadricNormals(self.quadratic, GLU_SMOOTH)
         gluQuadricTexture(self.quadratic, GL_TRUE)
         self.texture = LoadGLTextures('../data/world.bmp')
+        self.oldRotationMatrix = None
+        self.resetMatrix = None
+    
+    def rotate(self, new_rotx, new_roty):
+        #print "NRX: ", new_rotx, "NRY: ", new_roty
+
+        glPushMatrix()
+
+        glLoadIdentity()
+        glRotatef(new_rotx, 1,0,0)
+        x_rot_matrix = (ctypes.c_double*16)()
+        glGetDoublev( GL_MODELVIEW_MATRIX, x_rot_matrix )
+
+        glLoadIdentity()
+        glRotatef(new_roty, 0,1,0)
+        y_rot_matrix = (ctypes.c_double*16)()
+        glGetDoublev( GL_MODELVIEW_MATRIX, y_rot_matrix )
+
+        glLoadIdentity()
+        glMultMatrixf( (ctypes.c_float*16)(*x_rot_matrix) )
+        glMultMatrixf( (ctypes.c_float*16)(*y_rot_matrix) )
+        temp_rot_matrix = (ctypes.c_double*16)()
+        glGetDoublev( GL_MODELVIEW_MATRIX, temp_rot_matrix )
+
+        glLoadIdentity()
+        if (self.oldRotationMatrix):
+            # load the old matrix
+            glMultMatrixf( (ctypes.c_float*16)(*self.oldRotationMatrix) )
+
+        # rotate by the new values
+        #glRotatef(new_rotx, 1,0,0)
+        #glRotatef(new_roty, 0,1,0)
+        #glRotatef(self.rotz, 0,0,1)
+        glMultMatrixf( (ctypes.c_float*16)(*temp_rot_matrix) )
+
+        # save the rotation matrix
+        self.oldRotationMatrix = (ctypes.c_double*16)()
+        glGetDoublev( GL_MODELVIEW_MATRIX, self.oldRotationMatrix )
+
+        #import sys
+        #for i in range(0, 16):
+        #    sys.stdout.write("%s," %(self.oldRotationMatrix[i]))
+        #sys.stdout.write("\n")
+
+        glPopMatrix()
+        # get original sphere transform matrix
+        #var tempMatrix:Matrix3D = sphere.transform;
         
+        # create the rotation Matrixes for each local rotation axis given the corresponding X & Y
+        # distances where the user rolled over on the surface of the sphere
+        # Note: The value passed to Matrix3D.rotationX() should really be radians but I'm just using the x & y values 
+        # divided by rotSpeedRatio which is a constant to bring the values down to "radiany" magnitudes 
+        # and thus decrease the speed of the rotation. this will have to be the variable to work with 
+        #when I implement the slowing down of the rotation.
+        ##var rotX:Matrix3D = Matrix3D.rotationX(-[mouse Y here]/[rotSpeedRatio]);
+        ##var rotY:Matrix3D = Matrix3D.rotationY(-[mouse X here]/[rotSpeedRatio]);
+
+        #Mix both Matrixes together
+        ##var rotResult:Matrix3D = Matrix3D.multiply(rotX,rotY);
+        #Apply (Multiply) the final transform matrix to the original one and assign it to the sphere transform
+        ##sphere.transform = Matrix3D.multiply(rotResult,tempMatrix);
+        #pass
+    
     def draw(self):
+        glPushMatrix()
+        glLoadIdentity()
         glTranslatef(0.0,0.0,-30)
+
+        # rotate with save matrix
+        if (self.oldRotationMatrix):
+            glMultMatrixf( (ctypes.c_float*16)(*self.oldRotationMatrix) )
+
         glBindTexture(self.texture.target, self.texture.id)
         gluSphere(self.quadratic,self.radius,32,32)
-
+        glPopMatrix()
 
