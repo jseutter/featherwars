685e1f5137506e5595d9fd7a262910514b9894b6
bdsouza <bdsouza@bdsouza-vm1.int.pason.com>
2009-09-04 18:13:00 -0600

    world is in!

---
+++ FOWL/src/world.py
@@ -0,0 +1,22 @@
+from pyglet.gl import *
+import math
+from helper import LoadGLTextures
+
+
+
+class World:
+    worldTexture=None
+    
+    def __init__(self):
+        self.radius = 15
+        self.quadratic=gluNewQuadric()
+        gluQuadricNormals(self.quadratic, GLU_SMOOTH)
+        gluQuadricTexture(self.quadratic, GL_TRUE)
+        self.texture = LoadGLTextures('world2.bmp')
+        
+    def draw(self):
+        glTranslatef(0.0,0.0,-30)
+        glBindTexture(self.texture.target, self.texture.id)
+        gluSphere(self.quadratic,self.radius,32,32)
+
+
